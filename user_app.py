656f64414d20694fe58200f521477ad57ac612a7
rastapar <roman.chernysh@ua.es>
2024-05-08 11:25:23 +0200

    quantization of gemma2b

--- user_app.py
+++ user_app.py
@@ -294,5 +294,5 @@ with gr.Blocks(fill_height=True, theme=gr.themes.Base(), css=css, js=js, head=he
 
 
 if __name__ == '__main__':
-    is_public_link = False
+    is_public_link = True
     demo.launch(share=is_public_link)
